3588e859e2beeaae6fdeea7d7e7843e8f1b58743
heyuyi <1244620185@qq.com>
2016-08-04 12:40:40 +0800

    2016-8-4

--- demo/main.cpp
+++ demo/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+//    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
     MainDialog w;
     w.show();
 
